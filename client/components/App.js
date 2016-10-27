1f09768a3e079d674d8792a2d1ea3427b757f882
 Sentiurin Vladimir <sentiurinvladimir@Sentiurins-MacBook-Pro.local>
2016-10-27 18:54:40 +0300

    hardcode

--- client/components/App.js
+++ client/components/App.js
@@ -46,7 +46,7 @@ class App extends React.Component {
           }, {
             className: 'col-md-4',
             widgets: [{key: 'test3'}],
-          },{
+          }, {
             className: 'col-md-4',
             widgets: [{key: 'test'}],
           }, {
@@ -61,10 +61,10 @@ class App extends React.Component {
     };
   }
 
-  updateState(e) {
+  updateState(layout) {
     var updatedColumns = [];
 
-    e.rows.map(row => {
+    layout.rows.map(row => {
       row.columns.map(col => {
         updatedColumns.push(col);
       });
@@ -79,12 +79,12 @@ class App extends React.Component {
     });
   }
 
-  drag(e) {
-    this.updateState(e);
+  drag(layout) {
+    this.updateState(layout);
   }
 
-  remove(e) {
-    this.updateState(e);
+  remove(layout) {
+    this.updateState(layout);
   }
 
   edit() {
@@ -94,15 +94,65 @@ class App extends React.Component {
       this.setState({ editable: true });
   }
 
-  add(e, row, col) {
+  add(layout, row, col) {
     var obj = {key: 'test3'};
-
     var rows = this.state.layout.rows;
-
     var target = rows[row].columns[col];
+
     target.widgets.push(obj);
 
-    this.updateState(e);
+    this.updateState(layout);
+  }
+
+  componentDidMount() {
+    this.editableColumns = document.querySelectorAll('.editable-column');
+    var container = this.editableColumns[0].closest('.row').clientWidth;
+
+    var colSize = (size) => {
+      var s = 8.33333333333 * size;
+      return Number((container * s / 100).toFixed(2))
+    };
+
+    this.editableColumns.forEach(col => {
+      col.style.resize = 'both';
+      col.style.overflow = 'auto';
+      col.style.minWidth = '1px';
+      var count = 4;
+      var drag = {
+        begin: null,
+        end: null,
+      };
+
+      col.addEventListener('mouseup', (e) => {
+        var that = e.target.closest('.editable-column');
+        drag.end = that.clientWidth;
+
+        that.style.width = '';
+        that.classList.remove('col-md-' + count);
+
+        for (let i = 0; i < 12; i++) {
+          if (drag.end > colSize(i - 1) && drag.end < colSize(i + 1) && count < 12) {
+            count = i;
+          } else if (drag.end > drag.begin && drag.end >= container) {
+            count = 12;
+          } else if (drag.end > drag.begin && count > 10 && count < 12) {
+            count++;
+          }
+        }
+
+        if (drag.end < drag.begin && count > 0) {
+          count--;
+        }
+
+        that.classList.add('col-md-' + count);
+      });
+
+      col.addEventListener('mousedown', (e) => {
+        var that = e.target.closest('.editable-column');
+        drag.begin = that.clientWidth;
+      });
+
+    });
   }
 
   render() {
@@ -110,7 +160,8 @@ class App extends React.Component {
       <div className="container" id="app">
         <button onClick={this.edit.bind(this)}>edit</button>
         <br/>
-        <Dashboard editable={this.state.editable}
+        <Dashboard ref="dashboard"
+                   editable={this.state.editable}
                    onMove={this.drag.bind(this)}
                    onRemove={this.remove.bind(this)}
                    onAdd={this.add.bind(this)}
